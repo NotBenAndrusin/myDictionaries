ed203739ea99308e2f3224fab6e12ec2709f4fa2
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2022-01-26 12:29:05 -0600

    initial commit

---
+++ dictionary start file.py
@@ -0,0 +1,147 @@
+phonebook = {'Chris':'555−1111',
+             'Katie':'555−2222',
+             'Joanne':'555−3333'}
+
+print()
+print('*****  start section 1 - print dictionary ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 1 ********')
+print()
+
+
+
+
+
+
+
+
+print()
+print('*****  start section 2 - search dictionary ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  end section 2 ********')
+print()
+
+
+
+
+
+
+
+print()
+print('*****  start section 3 - edit/append dictionary ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 3 ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  start section 4 - delete/remove from dictionary ********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 4 ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  start section 5 - iterate through keys ********')
+print()
+
+
+
+
+print()
+print('*****  end section 5 ********')
+print()
+
+
+
+
+
+
+print()
+print('*****  start section 6 - iterate through values  ********')
+print()
+
+
+
+
+print()
+print('*****  end section 6 ********')
+print()
+
+
+
+
+
+
+
+
+print()
+print('*****  start section 7 - iterate through both key and value pair********')
+print()
+
+
+
+
+
+print()
+print('*****  end section 7 ********')
+print()
+
+
+
+
+
+
+
+
+print()
+print('*****  start section 8 - using random and converting to list ********')
+print()
+
+
+
+
+print()
+print('*****  end section 8 ********')
+print()
+
+
+
+
+
+
+
+
---
+++ person_dict.py
@@ -0,0 +1,30 @@
+person = {}
+person['fname'] = 'Joe'
+person['lname'] = 'Fonebone' 
+person['age'] = 51
+person['spouse'] = 'Edna'
+person['children'] = ['Ralph', 'Betty', 'Joey']
+person['pets'] = {'dog': 'Fido', 'cat': 'Sox'} 
+
+#print(person)
+
+#print(type(person['children']))
+
+
+#print(person['children'][2])
+
+
+#print(person['pets']['cat'])
+
+
+#for i in person['children']:
+#     print(i)
+
+for i,j in person['pets'].items():
+     print(i)
+     
+
+
+
+
+                      
