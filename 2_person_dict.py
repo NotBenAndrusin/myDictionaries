eba3d098ef3d853da368ac47d6c2ed28b0809239
Johnny Bhojwani <johnnyvb@gmail.com>
2022-09-05 16:20:01 -0500

    created person dictionary key

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,27 +5,3 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
-
-<<<<<<< HEAD:2_person_dict.py
-
-
-
-
-                      
-=======
-# print(person)
-
-# print(type(person["children"]))
-
-# print (type(person["pets"]))
-
-# print child - "Betty"
-
-# print the pet name of the cat
-
-
-# use a for loop to print out all the children
-
-
-# use a for loop to print out the type of pet and name of pet
->>>>>>> 8d356034c3abd1e37c436745f343a43ae109866a:1a_person_dict.py
