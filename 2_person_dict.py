4c0400776b2cc7e31e5b628727f985909f38b86c
Johnny Bhojwani <johnnyvb@gmail.com>
2022-09-05 16:13:54 -0500

    updated project

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -6,23 +6,6 @@ person['spouse'] = 'Edna'
 person['children'] = ['Ralph', 'Betty', 'Joey']
 person['pets'] = {'dog': 'Fido', 'cat': 'Sox'} 
 
-#print(person)
-
-#print(type(person['children']))
-
-
-#print(person['children'][2])
-
-
-#print(person['pets']['cat'])
-
-
-#for i in person['children']:
-#     print(i)
-
-for i,j in person['pets'].items():
-     print(i)
-     
 
 
 
